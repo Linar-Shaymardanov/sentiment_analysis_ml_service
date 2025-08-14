50d9c37472c021ffdbe05d437d88ea32216d56be
Linar-Shaymardanov <linar.shaymardanov.mu@gmail.com>
2025-08-14 22:16:44 +0330

    fix(db): provide get_database_engine, engine, get_session and init_db

--- app/database/database.py
+++ app/database/database.py
@@ -1,27 +1,22 @@
 # app/database/database.py
 from sqlmodel import SQLModel, Session, create_engine
+from contextlib import contextmanager
 from .config import get_settings
 
 def get_database_engine():
     settings = get_settings()
-    engine = create_engine(
-        url=settings.DATABASE_URL_psycopg,
-        echo=settings.DEBUG,
-        pool_size=5,
-        max_overflow=10,
-        pool_pre_ping=True,
-        pool_recycle=3600
-    )
+    engine = create_engine(settings.DATABASE_URL_psycopg, echo=settings.DEBUG)
     return engine
 
+# engine для общего использования
 engine = get_database_engine()
 
-def get_session():
-    with Session(engine) as session:
-        yield session
-
-def init_db(drop_all: bool = False) -> None:
-    engine = get_database_engine()
+def init_db(drop_all: bool = False):
     if drop_all:
         SQLModel.metadata.drop_all(engine)
     SQLModel.metadata.create_all(engine)
+
+# FastAPI dependency
+def get_session():
+    with Session(engine) as session:
+        yield session
